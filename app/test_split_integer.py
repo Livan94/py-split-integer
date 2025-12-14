8e6c3aa6909a3c8f200ef9294a5259701e75721e
Oleh Klivanets <oleg.klivanets@gmail.com>
2025-12-14 15:20:13 +0200

    Simplify split_integer tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,11 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 777
-    parts = 30
-
-    result = split_integer(value, parts)
-    assert sum(result) == value
+    assert sum(split_integer(777, 30)) == 777
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -20,15 +16,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(23, 3)
-    assert result == sorted(result)
-
-    assert max(result) - min(result) <= 1
+    assert split_integer(23, 3) == [7, 8, 8]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(6, 8)
-    assert len(result) == 8
-    assert sum(result) == 6
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
+    assert split_integer(6, 8) == [0, 0, 1, 1, 1, 1, 1, 1]
